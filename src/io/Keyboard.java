f481fcaae4eed427496d51d5f7ed3b76ee873ea6
bobby-bouche <n_8-bit_Bobby@hotmail.com>
2025-10-23 20:59:48 -0700

    created constructor for keyboard

--- src/io/Keyboard.java
+++ src/io/Keyboard.java
@@ -1,5 +1,16 @@
 package io;
 
+import java.util.Scanner;
+
 public class Keyboard {
+	
+	// fields
+	Scanner scanner;
+	
+	
+	// constructor
+	public Keyboard() {
+		scanner = new Scanner(System.in);
+	}
 
 }
