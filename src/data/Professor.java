c34c5fc4f63af9ba31bf566aef1d3563c740522b
bobby-bouche <n_8-bit_Bobby@hotmail.com>
2025-10-26 20:16:28 -0700

    created professor class

--- src/data/Professor.java
+++ src/data/Professor.java
@@ -2,4 +2,66 @@ package data;
 
 public class Professor extends Person {
 
+	// professor fields
+	private String subject;
+	private String rank;
+	
+	
+	// symbolic constants
+	
+	
+	
+	// constructors
+	public Professor() {
+		super();
+	}
+	
+	public Professor(int id, String name, String subject, String rank) {
+		super(id,name);
+		validateSubject(subject);
+		this.subject = subject;
+		validateRank(rank);
+		this.rank = rank;
+	}
+	
+	
+	
+	// validators
+	private static void validateSubject(String subject) {
+		
+	}
+	
+	private static void validateRank(String rank) {
+		
+	}
+	
+	
+	// getters and setters
+	public String getSubject() {
+		return subject;
+	}
+	
+	public void setSubject(String subject) {
+		validateSubject(subject);
+		this.subject = subject;
+	}
+	
+	public String getRank() {
+		return rank;
+	}
+	
+	public void setRank(String rank) {
+		validateRank(rank);
+		this.rank = rank;
+	}
+
+	
+	
+	// toString
+	@Override
+	public String toString() {
+		return "Professor [subject=" + subject + ", rank=" + rank + "]";
+	}
+
+	
 }
--- src/data/Student.java
+++ src/data/Student.java
@@ -1,5 +1,5 @@
 package data;
 
-public class Student {
+public class Student extends Person {
 
 }
