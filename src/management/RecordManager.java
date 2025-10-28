35f4f698e003a87fdd8c32c699eecb7f86775a72
bobby-bouche <n_8-bit_Bobby@hotmail.com>
2025-10-27 21:03:46 -0700

    made initial headwway on recordManager class

--- src/management/RecordManager.java
+++ src/management/RecordManager.java
@@ -1,7 +1,28 @@
 package management;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 import data.Person;
 
 public class RecordManager<T extends Person> {
 
+	private Map<Integer, T> records = new HashMap<>();
+	
+	
+	
+	public void add(T entry) {
+		records.put(entry.getSchool_id(), entry);
+	}
+	
+	
+	
+	public List<T> all(){
+		return new ArrayList<>(records.values());
+	}
+	
+	
 }
