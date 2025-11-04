0ead35176d1ddb310fca6d9f223b43dc0f2e3efd
bobby-bouche <n_8-bit_Bobby@hotmail.com>
2025-11-03 21:09:26 -0800

    added requirment note for reportGenerator

--- src/analytics/ReportGenerator.java
+++ src/analytics/ReportGenerator.java
@@ -1,5 +1,15 @@
 package analytics;
 
 public class ReportGenerator {
+	
+	/*
+	 *   must implement following
+	 * 
+	 * - Top N students overall by GPA
+	 * - Average GPA per course
+	 * - Most popular course(s)
+	 * - Search: find students by partial name (case-insensitive) using streams
+	 * 
+	 */
 
 }
