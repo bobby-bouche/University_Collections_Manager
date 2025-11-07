5f3883e94f22876a957eebefef1a009b7bf45c35
bobby-bouche <n_8-bit_Bobby@hotmail.com>
2025-11-06 22:22:02 -0800

    added new topStudents method for reportgenerator class to return the top N rated students

--- src/analytics/ReportGenerator.java
+++ src/analytics/ReportGenerator.java
@@ -1,6 +1,15 @@
 package analytics;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import data.Student;
+import management.RecordManager;
+
 public class ReportGenerator {
+	
+	RecordManager students = new RecordManager();
 	
 	/*
 	 *   must implement following
@@ -24,5 +33,13 @@ public class ReportGenerator {
 	
 	public void mostPopularCourse() {
 		
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public List<Student> topStudents(int num){
+		//kb.validateInt(num);
+		List<Student> topRated = (List<Student>) students.all().stream().sorted(Comparator.comparingDouble(Student::getGpa).reversed()).limit(num).collect(Collectors.toList());
+		return topRated;
 	}
 }
